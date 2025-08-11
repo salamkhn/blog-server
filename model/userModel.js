6f6b4e5a8391978580b1c29252d6b32031b3aae3
salamkhan <motivation3272@gmail.com>
2025-08-11 04:46:15 -0700

    userModel created

---
+++ model/userModel.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose"
+
+
+//user Schema
+const userSchema=mongoose.Schema({
+  userName:{
+    type:String,
+    required:[true,'userName required'],
+    minlength:[3,'userName atleast 3 character long'],
+    maxlength:[25,'userName cannot exceed 25 character']
+  },
+  email:{
+    type:String,
+    lowercase:true,
+    required:[true,'Email required'],
+    unique:true,
+    match:/^[\w]+(?:[-_.][\w]+)*@[a-z0-9]+(?:[-_.][\w]+)*\.[A-Za-z]{2,}$/
+
+  },
+  password:{
+    type:String,
+    minlength:[6,"password should be 6 char long"],
+    required:[true,'password required'],
+    select:false
+  },
+  phoneNumber:{
+    type:Number,
+    required:[true,'phoneNumber required and must be in digit'],
+    match:/^\+?[0-9]+$/
+    
+  },
+  role:{
+    type:String,
+    enum:{
+      values:["admin","user"],
+      message:"{VALUE} is not valide"
+    },
+    default:"user"
+  },
+  userprofile:{
+    type:String,
+    required:[true,"userProfile required"],
+    validate:{
+     validator:function(v){
+      return /\.(png|jpg|jpeg|webp)$/i.test(v)
+     },
+     message:props=>`${props.value} is not a valid image extension' (png,jpg,jpeg,webp only allowed)`
+    }
+  }
+
+},{timestamps:true})
+
+//userModle
+const User=mongoose.model("User",userSchema)
+
